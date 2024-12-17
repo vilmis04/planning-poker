8e28b904bf94c367119c1eb5df9145d88e053257
Vilmantas <vilmantas.sudaris@gmail.com>
2024-12-17 23:27:01 +0200

    initial

---
+++ apps/planning-poker-service/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'planning-poker-service',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/planning-poker-service',
+};
